48c1a147b310ce42456cf1f8d0f194e1c299782c
u-ever <uever.martins@gmail.com>
2021-08-26 21:45:29 -0300

    notacoes

--- js/darkmode.js
+++ js/darkmode.js
@@ -6,5 +6,13 @@ checkbox.addEventListener('click', function() {
 	html.classList.toggle('dark-mode')
 })
 
+
+
 // necessário salvar no Local Storage 
+/*
+if (typeof(Storage) !== "undefined") {
+	localStorage.setItem("lightmode", 0)
+}
+*/
+
 // 	checkbox.innerHTML = '<input type="checkbox" id="switch"> Light Mode'
