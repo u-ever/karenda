c28fc1bc4ed8aae76ef121826bf5c6e9738452e6
u-ever <uever.martins@gmail.com>
2021-08-29 20:47:02 -0300

    prompt

--- js/calendar-test.js
+++ js/calendar-test.js
@@ -1,15 +1,21 @@
 //calendar-----------------------------------------------
 
 function calendar() {
-    const days = document.querySelector(".day-indice");
+    var days = document.querySelector(".day-indice");
  
     for (let day = 1; day <= 35; day++) {
        if (day <= 31) {
-        days.innerHTML += ` <div onclick="prompt('Digite o título do evento')"><time datetime="2021-08-${day}">${day}</time></div> `
+        days.innerHTML += `<div id="day${day}" onclick="exibirEvento()"><time datetime="2021-08-${day}">${day}</time></div> `
        } else {
         days.innerHTML += `<div style = "background-color: gray; color: white"><time datetime="2021-08-${day - 31}">${day - 31}</time></div> `
        }
     }
  }
  
- window.onload = calendar()
+ window.onload = calendar();
+
+ //Pronpt
+
+ function exibirEvento() {
+   var tituloEvento = prompt("Digite o título do evento")
+ }
