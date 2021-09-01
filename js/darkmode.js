d07113cf6c8dd0cb5322460265b1590dbaba4f6b
u-ever <uever.martins@gmail.com>
2021-09-01 00:41:06 -0300

    darkmode-success

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,25 +1,19 @@
 //dark-mode-----------------------------------------------
 const html = document.querySelector('html')
 const btntheme = document.querySelector('#switch-dark')
-const btntheme = document.querySelector('#switch-light')
 
-btntheme.addEventListener('click', function() {
-	html.classList.toggle('dark-mode')
-	localStorage.setItem("theme", "dark-mode")
-	
+btntheme.addEventListener('click', function () {
+	if (theme == "dark-mode") {
+		html.classList.toggle(theme)
+		localStorage.removeItem("theme")
+	} else {
+		html.classList.toggle('dark-mode')
+		localStorage.setItem("theme", "dark-mode")
+	}
 })
 
 
 let theme = localStorage.getItem("theme")
-html.classList.toggle(theme)
-console.log(theme)
-
-
-// necessário salvar no Local Storage 
-/*
-if (typeof(Storage) !== "undefined") {
-	localStorage.setItem("lightmode", 0)
-}
-*/
-
-// 	checkbox.innerHTML = '<input type="checkbox" id="switch"> Light Mode'
+if (theme == "dark-mode") {
+	html.classList.toggle(theme)
+} 
